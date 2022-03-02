5335e11043ad506fa2855289e1b4f6b234bfc11d
rylee thomas <ryleetreat@gmail.com>
2022-03-01 23:53:01 -0500

    update backend comment reply

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -6,10 +6,10 @@ const pizzaController = {
     Pizza.find({})
       .populate({
         path: 'comments',
-        select:'-__v'
+        select: '-__v'
       })
       .select('-__v')
-      .sort({_id: -1 })
+      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
@@ -22,7 +22,7 @@ const pizzaController = {
     Pizza.findOne({ _id: params.id })
       .populate({
         path: 'comments',
-        select:'-__v'
+        select: '-__v'
       })
       .select('-__v')
       .then(dbPizzaData => res.json(dbPizzaData))
@@ -49,20 +49,14 @@ const pizzaController = {
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.status(400).json(err));
+      .catch(err => res.json(err));
   },
 
   // delete pizza
   deletePizza({ params }, res) {
     Pizza.findOneAndDelete({ _id: params.id })
-      .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch(err => res.status(400).json(err));
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
   }
 };
 
